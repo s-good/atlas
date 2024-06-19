023d3e76d68be7c67b87f683c7e0ad2a92eac258
s-good <s-good@users.noreply.github.com>
2024-06-20 00:28:25 +0100

    Generalise the method of dealing with east-west wrapping.

--- src/atlas/interpolation/method/unstructured/UnstructuredBilinearLonLat.cc
+++ src/atlas/interpolation/method/unstructured/UnstructuredBilinearLonLat.cc
@@ -419,9 +419,14 @@ Method::Triplets UnstructuredBilinearLonLat::projectPointToElements(size_t ip, c
             }
             // shift cells on the east-west periodic boundary from the east to the west
             // so that the quad surrounds a point with output longitude in [0,360)
-            if (lons[0] > lons[1] || lons[3] > lons[2]) {
-                lons[0] -= 360;
-                lons[3] -= 360;
+            double minlon = 9999;
+            for ( int i = 0; i < 4; i++ ) {
+                minlon = std::min( minlon, lons[i] );
+            }
+            for ( int i = 0; i < 4; i++ ) {
+                if ( (lons[i] - minlon) > 180 ) {
+                    lons[i] -= 360;
+                }
             }
 
             element::Quad2D quad(
